7df0e4282e161cc7d287ac228f520c01c7317674
Riley Stephens <riley.stephens28@gmail.com>
2018-04-27 14:43:25 -0500

    redid virtual env

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/rileystephens/Documents/Projects/F.A.M./venv"
+set -gx VIRTUAL_ENV "/Users/rileystephens/Documents/Projects/F.A.M./FAM/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
